75036b859d5ae2084cc6c1ac35a6043a7b3a0857
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-28 10:56:28 +0530

    Add : Class And Object Concept

---
+++ Day2Assignment/ObjectAndClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Assignment
+{
+    public class ObjectAndClass
+    {
+        public void ClassAndObject()
+        {
+            Console.WriteLine("Enter the first value : ");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Value : ");
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The Sum of two Value is : {first+second}");
+        }
+    }
+}
---
+++ Day2Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Day2Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            ObjectAndClass obj = new ObjectAndClass();
+            obj.ClassAndObject();
+            Console.ReadKey();
+        }
+    }
+}
