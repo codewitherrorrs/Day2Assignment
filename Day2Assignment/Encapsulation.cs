db3eb71780e82fab2b999e58f638b6d8b8656748
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-28 11:34:12 +0530

    Add : Encapsulation

---
+++ Day2Assignment/Encapsulation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Assignment
+{
+    public class Encapsulation
+    {
+        private string name = "Sagar";
+        private string standard = "10th";
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Standard
+        {
+            get;
+            set;
+        }
+    }
+}
--- Day2Assignment/Program.cs
+++ Day2Assignment/Program.cs
@@ -6,10 +6,13 @@ namespace Day2Assignment
     {
         public static void Main(string[] args) 
         {
-            MethodOverride obj = new MethodOverride();
+           /* MethodOverride obj = new MethodOverride();
             obj.Method();
             Derived obj_One = new Derived();
-            obj_One.Method();
+            obj_One.Method(); */
+            Encapsulation obj= new Encapsulation();
+            Console.WriteLine($"Name : {obj.Name = "Abhishek"}");
+            Console.WriteLine($"Standard : {obj.Standard = "Graduate"}");
             Console.ReadKey();
         }
     }
