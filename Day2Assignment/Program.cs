33ccc11c51c55d6b0439ff3fac7b3f1f0bb90186
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-28 12:24:43 +0530

    Add : Value And Reference Type

--- Day2Assignment/Program.cs
+++ Day2Assignment/Program.cs
@@ -6,19 +6,25 @@ namespace Day2Assignment
     {
         public static void Main(string[] args) 
         {
-           /* MethodOverride obj = new MethodOverride();
-            obj.Method();
-            Derived obj_One = new Derived();
-            obj_One.Method(); */
-         /*   Encapsulation obj= new Encapsulation();
-            Console.WriteLine($"Name : {obj.Name = "Abhishek"}");
-            Console.WriteLine($"Standard : {obj.Standard = "Graduate"}");
-         */
-            TypesOFMethod obj = new TypesOFMethod();
-            obj.Method();
-            obj.Method_One("Shreya","Ma'am");
-            Console.WriteLine(obj.Method_Two("Abhishek","Tara"));
-            Console.ReadKey();
+            /* MethodOverride obj = new MethodOverride();
+             obj.Method();
+             Derived obj_One = new Derived();
+             obj_One.Method(); */
+            /*   Encapsulation obj= new Encapsulation();
+               Console.WriteLine($"Name : {obj.Name = "Abhishek"}");
+               Console.WriteLine($"Standard : {obj.Standard = "Graduate"}");
+            */
+            /* TypesOFMethod obj = new TypesOFMethod();
+             obj.Method();
+             obj.Method_One("Shreya","Ma'am");
+             Console.WriteLine(obj.Method_Two("Abhishek","Tara"));
+             Console.ReadKey(); */
+            //Reference And Value type.
+            int value = 20;
+            int value_One = 40;
+            ValueAndRefType obj = new ValueAndRefType();
+            obj.Method(value,value_One);
+            obj.Method_One(ref value,value_One);
         }
     }
 }
---
+++ Day2Assignment/ValueAndRefType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Assignment
+{
+    public class ValueAndRefType
+    {
+        //Value Type
+        public void Method(int first,int second)
+        {
+            Console.WriteLine($"Sum : {first + second}");
+        }
+
+        //Reference Type  
+        public void Method_One(ref int first , int second)
+        {
+            Console.WriteLine($"Reference Sum : {first + second}");
+        }
+    }
+}
