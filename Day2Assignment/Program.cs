0750db2d8e130f527939a7c70024a9288072b5ab
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-28 11:57:36 +0530

    Add : Types of Method

--- Day2Assignment/Program.cs
+++ Day2Assignment/Program.cs
@@ -10,9 +10,14 @@ namespace Day2Assignment
             obj.Method();
             Derived obj_One = new Derived();
             obj_One.Method(); */
-            Encapsulation obj= new Encapsulation();
+         /*   Encapsulation obj= new Encapsulation();
             Console.WriteLine($"Name : {obj.Name = "Abhishek"}");
             Console.WriteLine($"Standard : {obj.Standard = "Graduate"}");
+         */
+            TypesOFMethod obj = new TypesOFMethod();
+            obj.Method();
+            obj.Method_One("Shreya","Ma'am");
+            Console.WriteLine(obj.Method_Two("Abhishek","Tara"));
             Console.ReadKey();
         }
     }
---
+++ Day2Assignment/TypesOFMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Assignment
+{
+    public class TypesOFMethod
+    {
+        //Method Without Parameter.
+        public void Method()
+        {
+            string firstName = "Sagar";
+            string secondName = "Shahu";
+            Console.WriteLine($"Full Name : {firstName +" "+ secondName}");
+        }
+        //Method With Parameter.
+        public void Method_One(string firstName, string secondName)
+        {
+            Console.WriteLine($"Full Name : {firstName} {secondName}");
+        }
+        //Method With Return Type.
+        public string Method_Two(string firstName, string secondName)
+        {
+            return "Full Name : "+firstName +" "+secondName;
+        }
+    }
+}
